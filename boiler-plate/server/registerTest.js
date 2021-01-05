6c0e51a284fbd00ac77fd32b8863070e675b2dad
0731a <0731SeoYeong@gmail.com>
2021-01-06 03:20:03 +0900

    [FIX] sequelize를 통한 모델생성과 데이터베이스 연결 확인..........

--- boiler-plate/server/registerTest.js
+++ boiler-plate/server/registerTest.js
@@ -1,17 +1,32 @@
 const express = require('express');
-const router = express.Router();
 const models = require("./models");
+const app = express(); /* app 제작 */
+const port = 5500;
+const bodyParser = require('body-parser');
+
+//application/x-www-form-urlencoded 데이터를 분석해서 가져오게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json파일을 분석해서 가져올수 있게 함
+app.use(bodyParser.json());
+
 
 console.log("server start");
 
 
-router.get('/sign_up', function(req, res, next) {
+app.get('/sign_up', function(req, res, next) {
   console.log("sign in");
-  res.render("user/signup");
+  //res.render("user/signup");
+  models.user.create({
+    name: "안서영2",
+    email: "98_073@anver.com",
+    password: "1234"
+  })
 });
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-router.post("/sign_up", function(req,res,next){
+app.post("/sign_up", function(req,res,next){
   let body = req.body;
   console.log("sign in");
 
@@ -28,4 +43,8 @@ router.post("/sign_up", function(req,res,next){
   })
 })
 
-module.exports = router;
+app.listen(port, () => {
+    /*포트 3000에서 앱 실행*/
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
