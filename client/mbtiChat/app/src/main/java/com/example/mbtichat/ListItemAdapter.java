ade601550739df8529123c917eccfef52274d749
0731a <0731SeoYeong@gmail.com>
2021-01-07 21:00:03 +0900

    [ADD] CustomListView test

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ListItemAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mbtichat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListItemAdapter extends BaseAdapter {
+    ArrayList<ListItem> items = new ArrayList<ListItem>();
+    Context context;
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        context = parent.getContext();
+        ListItem listItem = items.get(position);
+
+        if( convertView == null ){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listview_item, parent, false);
+        }
+
+        TextView nameText = convertView.findViewById(R.id.name);
+        TextView phoneText = convertView.findViewById(R.id.phone);
+
+        nameText.setText(listItem.getName());
+        phoneText.setText(listItem.getPhone());
+        return convertView;
+    }
+
+    public void addItem(ListItem item){
+        items.add(item);
+    }
+}
--- client/mbtiChat/app/src/main/java/com/example/mbtichat/User.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/User.java
@@ -1,4 +1,8 @@
 package com.example.mbtichat;
 
 public class User {
+    private int idx;
+    private String name;
+    private String id;
+    
 }
