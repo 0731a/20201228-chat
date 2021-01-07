b9041ffded2e36b8a99ffaa58261809e9e546268
0731a <0731SeoYeong@gmail.com>
2021-01-07 19:16:46 +0900

    [ADD] custom list test

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ListItem.java
@@ -0,0 +1,26 @@
+package com.example.mbtichat;
+
+public class ListItem {
+    private String name;
+    private String phone;
+
+    ListItem(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
