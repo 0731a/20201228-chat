24a5c82101791ff67ec13e701a9a97b3efdf93eb
0731a <0731SeoYeong@gmail.com>
2021-01-08 20:12:17 +0900

    [ADD] mbti result 계산 메소드 추가

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/util/MbtiTest.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/util/MbtiTest.java
@@ -2,8 +2,28 @@ package com.example.mbtichat.util;
 
 public class MbtiTest {
     private ReadTxt readTxt;
+    private int eCount = 0;
+    private int sCount = 0;
+    private int tCount = 0;
+    private int jCount = 0;
+
 
     public MbtiTest(){
         readTxt.openFile("mbtitest.txt");
+    }
+
+    public String getTestResult(){
+        String result = "";
+
+        if(eCount>0) result += 'E' ;
+        else result += "I";
+        if(sCount>0) result += 'S' ;
+        else result += 'N';
+        if(tCount>0) result += 'T';
+        else result += 'F';
+        if(jCount>0) result +='J';
+        else result += 'P';
+
+        return result;
     }
 }
