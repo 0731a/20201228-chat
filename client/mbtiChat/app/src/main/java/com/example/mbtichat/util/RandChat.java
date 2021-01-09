46646a23efe99d8fa96815d05d72f3e9f146ca62
0731a <0731SeoYeong@gmail.com>
2021-01-10 01:28:19 +0900

    [ADD] randChat model

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/util/RandChat.java
@@ -0,0 +1,43 @@
+package com.example.mbtichat.util;
+
+import java.util.Date;
+
+public class RandChat {
+    private Date date;
+    private String writer;
+    private String mbti;
+    private String text;
+    private String Image;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getMbti() {
+        return mbti;
+    }
+
+    public void setMbti(String mbti) {
+        this.mbti = mbti;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
