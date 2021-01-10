0bd0dc44b79b70a9ed66e696bfcb695e1ee7c5f7
0731a <0731SeoYeong@gmail.com>
2021-01-10 19:47:37 +0900

    [ADD] sequelize model 추가 public_chat

---
+++ boiler-plate/server/models/public_chat.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    idx: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Mbti',
+    timestamps: false
+  });
+  return user;
+};
