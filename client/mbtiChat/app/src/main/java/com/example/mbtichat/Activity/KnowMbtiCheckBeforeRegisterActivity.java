6429a2c001585bfe83d1d4a277ea07f47224059f
0731a <0731SeoYeong@gmail.com>
2021-01-11 23:11:39 +0900

    [ADD] Know~~Activity 내부 버튼 intent 경로 설정

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/KnowMbtiCheckBeforeRegisterActivity.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/KnowMbtiCheckBeforeRegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.mbtichat.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,26 @@ public class KnowMbtiCheckBeforeRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knowmbticheckbeforeregister);
+
+        Button registerWithMbtiTest  = (Button) findViewById(R.id.registerWithMbtiTest);
+        Button registerWithOutMbtiTest  = (Button) findViewById(R.id.registerWithOutMbtiTest);
+
+        registerWithOutMbtiTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KnowMbtiCheckBeforeRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerWithMbtiTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KnowMbtiCheckBeforeRegisterActivity.this, MbtiTestActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
