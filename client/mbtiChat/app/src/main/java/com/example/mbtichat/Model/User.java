c674a55a899166de3ef309dbbda4d91a87414036
0731a <0731SeoYeong@gmail.com>
2021-01-13 23:17:37 +0900

    [ADD] public chat 글 작성 요청

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/User.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/User.java
@@ -1,6 +1,30 @@
 package com.example.mbtichat.Model;
 
 public class User {
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private int idx;
     private String  nickName;
     private String id;
