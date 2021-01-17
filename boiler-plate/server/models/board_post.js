b681adcb2c0be787483bad708ba36174de1e5159
0731a <0731SeoYeong@gmail.com>
2021-01-17 22:46:21 +0900

    [FIX] db board_post model 변경

--- boiler-plate/server/models/board_post.js
+++ boiler-plate/server/models/board_post.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       type: DataTypes.INTEGER
     },
-    description: {
+    text: {
       type: DataTypes.STRING,
       allowNull: true
     }
