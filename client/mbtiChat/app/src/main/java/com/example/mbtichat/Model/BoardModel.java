066e18c74798e7a9f553b95d6a09a460415eb819
0731a <0731SeoYeong@gmail.com>
2021-01-17 22:43:59 +0900

    [ADD] 안드로이드 board model 추가

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/BoardModel.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/BoardModel.java
@@ -1,4 +1,31 @@
 package com.example.mbtichat.Model;
 
 public class BoardModel {
+    private int idx;
+    private String name;
+    private String description;
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
