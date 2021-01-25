a7c6e25c8fd79d48eafd0e6c638d873c602051e3
0731a <0731SeoYeong@gmail.com>
2021-01-25 22:48:13 +0900

    [ADD] random chat activity 추가

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/randChatMatchingActivity.java
@@ -0,0 +1,97 @@
+package com.example.mbtichat.Activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.mbtichat.R;
+import com.example.mbtichat.Service.UserService;
+import com.example.mbtichat.Util.Config;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class randChatMatchingActivity extends AppCompatActivity {
+    EditText text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_matching);
+
+        text = (EditText)findViewById(R.id.text);
+        Button sendButton = (Button)findViewById(R.id.send);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestMatching(UserService.myUser.getIdx(), text.getText().toString());
+            }
+        });
+    }
+
+    public void requestMatching(int idx, String text ){
+        //url 요청주소 넣는 editText를 받아 url만들기
+        String url = Config.IP_ADDRESS+"/randomChat/randMessage";
+
+        //JSON형식으로 데이터 통신을 진행합니다!
+        JSONObject testjson = new JSONObject();
+        try {
+            //입력해둔 edittext의 id와 pw값을 받아와 put해줍니다 : 데이터를 json형식으로 바꿔 넣어주었습니다.
+            testjson.put("text", text);
+            testjson.put("idx",idx);
+
+            //이제 전송해볼까요?
+            final RequestQueue requestQueue = Volley.newRequestQueue(randChatMatchingActivity.this);
+            final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url,testjson, new Response.Listener<JSONObject>() {
+
+                //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Log.d("Test","데이터전송 성공");
+
+                        //받은 json형식의 응답을 받아
+                        JSONObject jsonObject = new JSONObject(response.toString());
+
+                        //key값에 따라 value값을 쪼개 받아옵니다.
+                        String resultIdx= jsonObject.getString("idx");
+                        Log.d("result",resultIdx);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    Toast.makeText(randChatMatchingActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            requestQueue.add(jsonObjectRequest);
+            Log.d("Test","요청 보냄");
+            //
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
