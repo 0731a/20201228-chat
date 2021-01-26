1cb1e8a5a9bd2f402c5a00ff88c4500aa0133dc7
0731a <0731SeoYeong@gmail.com>
2021-01-26 23:19:33 +0900

    [ADD] myChatList method 추가, sequelize 질의문 완성, 수신 데이터 처리 필요

--- boiler-plate/server/routers/random_chat.js
+++ boiler-plate/server/routers/random_chat.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 const models = require('../models');
 const sequelize = require('sequelize');
-
+const Op = sequelize.Op;
 
 
 router.get('/', function(req,res,next){
@@ -22,8 +22,9 @@ router.post('/randMessage', function(req,res,next){
     models.user.findAll({ order: sequelize.literal('rand()'),  attributes: ['idx'], limit: 3 }).then((encounter) => {
         idxList = []
         console.log(encounter);
+
         encounter.forEach(function(userItem){
-            if (userItem.dataValues.idx == user_idx ) continue;
+            
             idxList.push(userItem.dataValues.idx);
             
             let result = models.random_chat_matching.create({
@@ -31,7 +32,7 @@ router.post('/randMessage', function(req,res,next){
                 user_idx_second: userItem.dataValues.idx
             })
             
-            models.random_chat_matching.create({
+            let chatResult = models.random_chat_message.create({
                 writer: user_idx,
                 text: text,
                 matching_idx: result.dataValues.idx
@@ -39,6 +40,20 @@ router.post('/randMessage', function(req,res,next){
         });
     }); 
 
+});
+
+router.post('/myChatList', function(req,res,next){
+    console.log(req.body);
+    models.random_chat_matching.findAndCountAll({[Op.or]: [{user_idx_first: req.body.id}, {user_idx_second: req.body.id}]})
+    .then( function(data){
+        console.log(data.rows);
+        if( (data== null || data == undefined) === false)
+          res.json({count:data.count, message:"이미 사용중인 이메일 입니다."});
+        else
+        res.json({result:false, message:"사용가능한 이메일 입니다."});
+    }).catch(function(error){
+        console.log(error);
+    })
 });
 
 
