9cac4ce7437a9ac7f407d85d55cea50304228583
0731a <0731SeoYeong@gmail.com>
2021-01-27 22:42:30 +0900

    [ADD] 대략적인 라우터 리스트

--- boiler-plate/server/routers/board.js
+++ boiler-plate/server/routers/board.js
@@ -40,5 +40,42 @@ router.post('/getBoardList', function(req,res,next){
 })
 });
 
+router.post('/postInBoard', function(req,res,next){
+
+    models.mbti_type.findOne({while:{type:req.body.type}})
+    .then( function(data){
+       res.json({result:data.dataValues.type, message:"이미 사용중인 아이디 입니다."});
+ 
+ })
+ });
+
+router.post('/deleteInBoard', function(req,res,next){
+
+    models.mbti_type.findOne({while:{type:req.body.type}})
+    .then( function(data){
+       res.json({result:data.dataValues.type, message:"이미 사용중인 아이디 입니다."});
+ 
+})
+});
+
+router.post('/postInPost', function(req,res,next){
+
+    models.mbti_type.findOne({while:{type:req.body.type}})
+    .then( function(data){
+       res.json({result:data.dataValues.type, message:"이미 사용중인 아이디 입니다."});
+ 
+})
+});
+
+router.post('/deleteInPost', function(req,res,next){
+
+    models.mbti_type.findOne({while:{type:req.body.type}})
+    .then( function(data){
+       res.json({result:data.dataValues.type, message:"이미 사용중인 아이디 입니다."});
+ 
+})
+});
+ 
+
 
 module.exports = router;
