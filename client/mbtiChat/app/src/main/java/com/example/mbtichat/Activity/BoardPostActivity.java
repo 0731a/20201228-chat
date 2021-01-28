94847ffb248982c7e4d8c1391f419af8bd100705
0731a <0731SeoYeong@gmail.com>
2021-01-28 21:35:47 +0900

    [ADD] board post xml + activity

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/BoardPostActivity.java
@@ -0,0 +1,16 @@
+package com.example.mbtichat.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mbtichat.R;
+
+public class BoardPostActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_post);
+    }
+}
+
