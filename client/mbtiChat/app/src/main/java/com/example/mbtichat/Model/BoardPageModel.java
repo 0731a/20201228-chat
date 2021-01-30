8fd19309ffb0aa6fd344286a5560ea0f9cb7d87c
0731a <0731SeoYeong@gmail.com>
2021-01-30 23:59:07 +0900

    [FIX] BoardPageModel

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/BoardPageModel.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Model/BoardPageModel.java
@@ -4,7 +4,40 @@ import java.io.Writer;
 import java.util.Date;
 
 public class BoardPageModel {
-    Date date;
-    Writer writer;
-    int boardIdx;
+    private Date createdAt;
+    private Writer writer;
+    private int boardIdx;
+    private String text;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Writer getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    public int getBoardIdx() {
+        return boardIdx;
+    }
+
+    public void setBoardIdx(int boardIdx) {
+        this.boardIdx = boardIdx;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
