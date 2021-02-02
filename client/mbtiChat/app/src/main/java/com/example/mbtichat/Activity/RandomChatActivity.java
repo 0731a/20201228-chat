6f17fe7ac63fb233a9cf2658f6773c47f79ad864
0731a <0731SeoYeong@gmail.com>
2021-02-02 21:58:26 +0900

    [ADD]

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/RandomChatActivity.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/RandomChatActivity.java
@@ -1,6 +1,9 @@
 package com.example.mbtichat.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,24 @@ public class RandomChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_randomchat);
+
+        Button sendMessageButton = (Button) findViewById(R.id.sendMessage);
+        Button messageListButton = (Button) findViewById(R.id.messageList);
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RandomChatActivity.this,randChatMatchingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        messageListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RandomChatActivity.this,RandomChatListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
