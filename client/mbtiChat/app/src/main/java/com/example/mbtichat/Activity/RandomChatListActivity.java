255e565f94a9d7e5a449f726ff262736a92ad104
0731a <0731SeoYeong@gmail.com>
2021-02-02 22:01:54 +0900

    [ADD] random chat activity

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/RandomChatListActivity.java
@@ -0,0 +1,15 @@
+package com.example.mbtichat.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mbtichat.R;
+
+public class RandomChatListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_randomchat_list);
+    }
+}
