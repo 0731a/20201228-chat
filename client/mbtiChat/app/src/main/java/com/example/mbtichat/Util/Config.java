fcdce2f1510d4777a8ae081414249de684384b47
0731a <0731SeoYeong@gmail.com>
2021-02-22 23:57:35 +0900

    [FIX] alarm_matching xml

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Util/Config.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Util/Config.java
@@ -1,5 +1,5 @@
 package com.example.mbtichat.Util;
 
 public class Config {
-    public static final String IP_ADDRESS= "http://192.168.200.183:3000";
+    public static final String IP_ADDRESS= "http://192.168.200.165:3000";
 }
