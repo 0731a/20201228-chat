1e9b1970d4aa547daf8a8c594cedc69ed30024bb
0731a <0731SeoYeong@gmail.com>
2021-02-24 21:59:10 +0900

    [Android/ADD] 액티비티 Context 한정자

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ActivityContext.java
@@ -0,0 +1,12 @@
+package com.example.mbtichat.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+// 액티비티 Context 한정자
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityContext {
+}
