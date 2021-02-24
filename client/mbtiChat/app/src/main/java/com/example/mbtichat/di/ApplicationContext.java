8e332cd6e0b2b074ab4b4c742dec6b01141eeb3b
0731a <0731SeoYeong@gmail.com>
2021-02-24 21:59:34 +0900

    [ADD] 애플리케이션 Context 한정자

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ApplicationContext.java
@@ -0,0 +1,13 @@
+package com.example.mbtichat.di;
+
+import javax.inject.Qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+//어플리케이션 Context 한정자
+@Qualifier
+@Retention (RetentionPolicy.RUNTIME)
+public @interface class ApplicationContext {
+}
