0e23e0385893a551a2141cea5864038dea70d90d
0731a <0731SeoYeong@gmail.com>
2021-02-27 02:55:36 +0900

    [FIX] setter 제거

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/data/entity/Comment.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/data/entity/Comment.java
@@ -11,40 +11,20 @@ public class Comment {
         return postId;
     }
 
-    public void setPostId(long postId) {
-        this.postId = postId;
-    }
-
     public long getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
 }
