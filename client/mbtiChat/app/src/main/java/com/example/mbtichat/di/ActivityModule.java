5f2e09df36c1fee178bf125a05cf80f738c5719f
0731a <0731SeoYeong@gmail.com>
2021-02-26 20:37:40 +0900

    [FIX] MainActivityForMVVM -> HomeActivity

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ActivityModule.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ActivityModule.java
@@ -1,7 +1,17 @@
 package com.example.mbtichat.di;
 
+import com.example.mbtichat.Activity.HomeActivity;
+import com.example.mbtichat.ui.MainModule;
+
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityModule {
+    /*
+     * MainActivity를 위한 서브 컴포넌트를 정의한다.
+     */
+    @ActivityScope
+    @ContributesAndroidInjector(modules = MainModule.class)
+    abstract HomeActivity homeActivity();
 }
