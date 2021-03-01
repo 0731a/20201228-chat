471ea93589d37b01637920043effeaded177e618
0731a <0731SeoYeong@gmail.com>
2021-03-01 19:16:45 +0900

    [ADD] retrofit 생성 메소드 추가

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/di/AppModule.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/di/AppModule.java
@@ -11,6 +11,9 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module(includes = ViewModelModule.class)
 public class AppModule {
@@ -34,6 +37,17 @@ public class AppModule {
     @Named("errorEvent")
     SingleLiveEvent<Throwable> provideErrorEvent(){
         return new SingleLiveEvent<>();
+    }
+
+    // Retrofit 객체 생성
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(){
+        return new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
     }
 
 }
