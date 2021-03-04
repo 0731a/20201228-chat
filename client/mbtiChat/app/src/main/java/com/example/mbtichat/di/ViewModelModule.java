14d9f192e6a39457f6a75259873b18f2cba24dba
0731a <0731SeoYeong@gmail.com>
2021-03-04 21:58:12 +0900

    [ADD] PostModule Bind

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ViewModelModule.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/di/ViewModelModule.java
@@ -1,13 +1,22 @@
 package com.example.mbtichat.di;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mbtichat.ui.post.PostViewModel;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 // ViewModel과 관련된 내용을 오브젝트 그래프로 관리
 @Module
 public abstract class ViewModelModule {
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(AppViewModelFactory factory);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(PostViewModel.class)
+    abstract ViewModel bindPostViewModel(PostViewModel viewModel);
 }
