6313e07d72f5cee44eb964432180a68f3acf43c4
0731a <0731SeoYeong@gmail.com>
2021-03-04 21:58:35 +0900

    [ADD] Bind PostModule

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/MainModule.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/MainModule.java
@@ -9,9 +9,13 @@ import com.example.mbtichat.R;
 import com.example.mbtichat.databinding.ActivityHomeBinding;
 import com.example.mbtichat.di.ActivityContext;
 import com.example.mbtichat.di.ActivityScope;
+import com.example.mbtichat.di.FragmentScope;
+import com.example.mbtichat.ui.post.PostFragment;
+import com.example.mbtichat.ui.post.PostModule;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class MainModule {
@@ -27,4 +31,12 @@ public abstract class MainModule {
     static Context provideContext(HomeActivity activity){
         return activity;
     }
+
+
+    /*
+     * 서브 컴포넌트 정의
+     */
+    @FragmentScope
+    @ContributesAndroidInjector(modules = PostModule.class)
+    abstract PostFragment getPostFragment();
 }
