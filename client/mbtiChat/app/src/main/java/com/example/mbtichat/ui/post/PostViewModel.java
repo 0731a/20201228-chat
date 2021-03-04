061f7a2527cdf06f042aff9f6be061123f15b75c
0731a <0731SeoYeong@gmail.com>
2021-03-04 21:59:22 +0900

    [ADD] PostViewModel

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/post/PostViewModel.java
@@ -0,0 +1,27 @@
+package com.example.mbtichat.ui.post;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import javax.inject.Inject;
+
+public class PostViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<Boolean> loading = new MutableLiveData<>(true);
+
+    @Inject
+    public PostViewModel(@NonNull Application application){
+        super(application);
+    }
+
+    public void loadPosts(){
+
+    }
+
+    public MutableLiveData<Boolean> getLoading(){
+        return loading;
+    }
+}
