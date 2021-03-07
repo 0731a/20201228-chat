d26dc23b2a7bef7a4869aeb07eef93c61cb7e680
0731a <0731SeoYeong@gmail.com>
2021-03-07 22:48:03 +0900

    [ADD] onViewCreated 메소드

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/post/PostFragment.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/post/PostFragment.java
@@ -9,9 +9,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.mbtichat.databinding.FragmentPostBinding;
 import com.example.mbtichat.di.AppViewModelFactory;
@@ -20,6 +23,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 
+import dagger.Lazy;
 import dagger.android.support.DaggerFragment;
 
 public class PostFragment extends DaggerFragment {
@@ -32,6 +36,14 @@ public class PostFragment extends DaggerFragment {
     AppViewModelFactory viewModelFactory;
 
     PostViewModel viewModel;
+
+    @Inject
+    PostAdapter adapter;
+    @Inject
+    LinearLayoutManager layoutManager;
+
+    @Inject
+    Lazy<NavController> navController;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
@@ -48,5 +60,22 @@ public class PostFragment extends DaggerFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @Nullable Bundle savedInstanceState){
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        // Lifecycle Owner 등록
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        // RecyclerView Adapter 지정
+        binding.recyclerView.setAdapter(adapter);
+        // RecyclerView 레이아웃 매니저 지정
+        binding.recyclerView.setLayoutManager(layoutManager);
+        // 바인딩 클래스에 ViewModel 연결
+        viewModel.getLivePosts()
+                .observe(getViewLifecycleOwner(), list -> adapter.setItems(list));
+        // 게시 글이 클릭되었을 때 게시 글 상세 화면 목적지로 이동
+        viewModel.getPostClickEvent()
+                .observe(getViewLifecycleOwner(), postItem -> navController.get().navigate(...));
     }
 }
