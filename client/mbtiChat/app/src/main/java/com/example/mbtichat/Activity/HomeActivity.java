389da0223c47b2d73d0917264617f6bced31df36
0731a <0731SeoYeong@gmail.com>
2021-03-07 22:49:38 +0900

    [ADD] 에러 이벤트 추가

--- client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/HomeActivity.java
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/Activity/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.mbtichat.Activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.mbtichat.databinding.ActivityHomeBinding;
+import com.example.mbtichat.util.SingleLiveEvent;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import dagger.Lazy;
 import dagger.android.support.DaggerAppCompatActivity;
@@ -16,14 +19,27 @@ public class HomeActivity extends DaggerAppCompatActivity {
     @Inject
     Lazy<ActivityHomeBinding> binding;
 
+    @Inject
+    @Named("errorEvent")
+    SingleLiveEvent<Throwable> errorEvent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         // 이 액티비티를 lifeCycleOwner로 설정하여,
         // 생명 주기에 안전하게 데이터 바인딩을 할 수 있도록 한다.
         binding.get().setLifecycleOwner(this);
+        errorEvent.observe(this, this::showErrorToast);
     }
 
+    private void showErrorToast(Throwable throwable)
+    {
+        throwable.printStackTrace();
+        showToast(throwable.getMessage());
+    }
 
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 
 }
