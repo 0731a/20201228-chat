a0ac0ff57ae91c1b958c75febcee415e7dd18b3d
0731a <0731SeoYeong@gmail.com>
2021-03-08 22:59:59 +0900

    [ADD] PostDetailUserItem

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/detail/PostDetailUserItem.java
@@ -0,0 +1,35 @@
+package com.example.mbtichat.ui.detail;
+
+import com.example.mbtichat.data.entity.User;
+import com.example.mbtichat.ui.post.PostItem;
+
+public class PostDetailUserItem extends PostDetailItem {
+    private User user;
+    private EventListener eventListener;
+
+    public PostDetailUserItem(User user, EventListener eventListener){
+        this.user = user;
+        this.eventListener = eventListener;
+    }
+
+    @Override
+    public Type getType(){
+        return Type.USER;
+    }
+
+    public String getName(){
+        return user.getName();
+    }
+
+    public long getUserId(){
+        return user.getId();
+    }
+
+    public EventListener getEventListener(){
+        return eventListener;
+    }
+
+    public interface EventListener {
+        void onUserClick(long userId);
+    }
+}
