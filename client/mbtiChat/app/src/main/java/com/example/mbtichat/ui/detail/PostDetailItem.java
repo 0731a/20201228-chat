2b054ef0dabaacc7a76ee90624458a813c408d26
0731a <0731SeoYeong@gmail.com>
2021-03-08 22:59:46 +0900

    [ADD] PostDetailItem

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/detail/PostDetailItem.java
@@ -0,0 +1,12 @@
+package com.example.mbtichat.ui.detail;
+
+
+public abstract class PostDetailItem {
+    public abstract Type getType();
+
+    public enum Type{
+        USER, // 사용자 정보
+        POST, // 게시 글
+        COMMENT // 댓글
+    }
+}
