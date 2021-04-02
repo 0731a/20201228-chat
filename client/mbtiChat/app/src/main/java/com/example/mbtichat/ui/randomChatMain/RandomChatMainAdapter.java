575e36f98a207fbb973cbbcf76287972cca47b4e
0731a <0731SeoYeong@gmail.com>
2021-04-02 22:14:12 +0900

    [ADD] random chat adapter

---
+++ client/mbtiChat/app/src/main/java/com/example/mbtichat/ui/randomChatMain/RandomChatMainAdapter.java
@@ -0,0 +1,59 @@
+package com.example.mbtichat.ui.randomChatMain;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.library.baseAdapters.BR;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mbtichat.R;
+import com.example.mbtichat.ui.post.PostItem;
+import com.example.mbtichat.util.ViewBindingHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class RandomChatMainAdapter extends RecyclerView.Adapter<ViewBindingHolder> {
+    // 뷰 홀더용 뷰 모델 리스트 변수
+    private final List<PostItem> items = new ArrayList<>();
+
+    // 생성자 인젝션
+    @Inject
+    public RandomChatMainAdapter(){
+    }
+
+    // 레이아웃 종류
+    @Override
+    public int getItemViewType(int position){
+        return R.layout.view_post;
+    }
+
+    // 뷰 홀더 생성
+    @NonNull
+    @Override
+    public ViewBindingHolder<?> onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        return new ViewBindingHolder(parent.getContext(), viewType);
+    }
+
+    // 뷰 홀더와 뷰 모델을 바인딩한다.
+    @Override
+    public void onBindViewHolder(@NonNull ViewBindingHolder holder, int position){
+        holder.getBinding().setVariable(BR.item, items.get(position));
+        holder.getBinding().executePendingBindings();
+    }
+
+    // 외부로부터 게시 글 목록을 받아서 UI를 갱신한다.
+    public void setItems(List<PostItem> items){
+        this.items.clear();
+        this.items.addAll(items);
+        this.notifyDataSetChanged();
+    }
+
+    // 게시 글 목록 수
+    @Override
+    public int getItemCount(){
+        return items.size();
+    }
+}
